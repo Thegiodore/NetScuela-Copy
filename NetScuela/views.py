c01c3904ae4a3bc12eaabb4e37bb1e2230c64efc
Thegiodore <tpsamson1@student.hau.edu.ph>
2023-11-11 21:30:26 +0800

    first commit

---
+++ NetScuela/views.py
@@ -0,0 +1,21 @@
+from django.shortcuts import render
+def home(request):
+    return render(request, 'home.html')
+
+def contact_us(request):
+    return render(request, 'contact_us.html')
+
+def about_us(request):
+    return render(request, 'about_us.html')
+
+def login(request):
+    return render(request, 'account/login.html')
+
+def register(request):
+    return render(request, 'account/register.html')
+
+def calendar(request):
+    return render (request, 'calendar.html')
+
+def account_settings(request):
+    return render (request, 'account_settings.html')
